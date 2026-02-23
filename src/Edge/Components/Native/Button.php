c6957920c40a0eb314c485ba0937b49fadadd958
Shane Rosenthal <srosenthal82@gmail.com>
2026-02-23 17:34:49 -0500

    Allow Button Blade component to use slot content as label
    Extracts text from slot content so <native:button>Click me</native:button>
    works the same as <native:button label=\"Click me\" />.

--- src/Edge/Components/Native/Button.php
+++ src/Edge/Components/Native/Button.php
@@ -2,12 +2,30 @@
 
 namespace Native\Mobile\Edge\Components\Native;
 
+use Native\Mobile\Edge\NativeElementCollector;
+
 class Button extends NativeBladeComponent
 {
-    protected bool $isSelfClosing = true;
+    protected bool $handlesCollectorManually = true;
 
     protected function elementType(): string
     {
         return 'button';
+    }
+
+    public function render(): \Closure
+    {
+        return function (array $data) {
+            $attrs = $data['attributes']->getAttributes();
+            $slot = trim(html_entity_decode(strip_tags($data['slot']->toHtml()), ENT_QUOTES, 'UTF-8'));
+
+            if ($slot !== '' && ! isset($attrs['label'])) {
+                $attrs['label'] = $slot;
+            }
+
+            NativeElementCollector::leaf($this->elementType(), $attrs);
+
+            return '';
+        };
     }
 }
